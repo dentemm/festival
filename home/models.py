7bcd6e9601744fa86053c15bcf1b81ff4c671b19
Tim Claes <tim.claes@me.com>
2016-03-03 10:12:48 +0100

    Started creating models for festival

--- home/models.py
+++ home/models.py
@@ -1,9 +1,33 @@
 from __future__ import unicode_literals
 
-from django.db import models
+from django.db import models as djangomodels
 
-from wagtail.wagtailcore.models import Page
+from wagtail.wagtailcore import blocks
+from wagtail.wagtailcore import fields
+from wagtail.wagtailcore import models
+from wagtail.wagtailimages.blocks import ImageChooserBlock
 
 
-class HomePage(Page):
-    pass
+# Global StreamField definitions
+
+# 1. Blocks used in StreamField
+
+class ImageWithCaptionBlock(blocks.StructBlock):
+	'''
+	Dit is een afbeelding met een caption text, kan ook als onderdeel gelden van een carousel
+	'''
+
+	image = ImageChooserBlock()
+	caption = blocks.TextBlock(required=False)
+
+
+class HomePageStreamBlock(blocks.StreamBlock):
+
+	carousel = blocks.ListBlock(ImageWithCaptionBlock(), template='home/blocks/carousel.html', icon='image')
+
+
+
+
+class HomePage(models.Page):
+
+	body = fields.StreamField(HomePageStreamBlock(), null=True)
