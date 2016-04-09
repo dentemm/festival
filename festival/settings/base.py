ee2770ced02caeb282af7a87e6e966ab74f1067d
Tim Claes <tim.claes@me.com>
2016-04-09 10:29:52 +0200

    Working Amazon S3 implemlentation!

--- festival/settings/base.py
+++ festival/settings/base.py
@@ -16,6 +16,9 @@ import os
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BASE_DIR = os.path.dirname(PROJECT_DIR)
 
+os.environ['S3_USE_SIGV4'] = 'True'
+os.environ['SWF'] = 'eu-central-1'
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
