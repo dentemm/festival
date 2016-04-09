f2355d9e27def6f27c60a8f5930a8ac987fc1372
Tim Claes <tim.claes@me.com>
2016-04-09 10:31:05 +0200

    production test

--- festival/settings/production.py
+++ festival/settings/production.py
@@ -33,3 +33,16 @@ COMPRESS_CSS_FILTERS = [
     'compressor.filters.cssmin.CSSMinFilter',
 ]
 COMPRESS_CSS_HASHING_METHOD = 'content'
+
+# AMAZON S3 STORAGE
+AWS_STORAGE_BUCKET_NAME = 'thinkmobile'
+AWS_REGION = 'eu-central-1'
+AWS_ACCESS_KEY_ID = 'AKIAJMC2L6WZ3TCR4RFQ'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_S3_HOST = 's3.eu-central-1.amazonaws.com'
+
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.eu-central-1.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+MEDIAFILES_LOCATION = 'media'
+MEDIA_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
+DEFAULT_FILE_STORAGE = 'home.custom_storages.MediaStorage'
