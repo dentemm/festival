9c2e14a4d21821c992f2b463f2ffdd57d2722931
Tim Claes <tim.claes@me.com>
2016-04-11 09:00:11 +0200

    Ready for comment implementation testing

--- comments/admin.py
+++ comments/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import CommentWithTitle
 
 # Register your models here.
+@admin.register(CommentWithTitle)
+
+class CommentWithTitleAdmin(admin.ModelAdmin):
+	pass
+
