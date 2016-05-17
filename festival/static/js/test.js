44a32a16141e69f3bc053fb96c28d1a9c3aca8b1
Tim Claes <tim.claes@me.com>
2016-05-17 20:19:10 +0200

    Almost ready with load more functionality

--- festival/static/js/test.js
+++ festival/static/js/test.js
@@ -3,17 +3,30 @@ $(document).ready(function () {
    	// Change appearance of selected category button and ajax call to fetch subcategories
 	$('#loadmore').click(function() {
 
-		ajax_call();
+		var count = $('div.festlist').size();
+
+		var page = count / 6;
+
+
+		console.log('count');
+		console.log(count);
+		console.log('page');
+		console.log(page)
+
+		ajax_call(page + 1);
 	});
 
-	function ajax_call() {
+	function ajax_call(page) {
 
 		console.log('JEEEEEEEEEJ');
 
 		$.ajax({
 			//url:"/products/overview/products/",
-			url:"/index/",
+			url:"/index/test/",
 			type: "GET",
+			data: {
+				'page': page,
+			},
 			success: my_handler,
 
 			error: function(xhr, ajaxOptions, thrownError) {
@@ -23,9 +36,12 @@ $(document).ready(function () {
 		});
 	};
 
-	function my_handler (data) {
+	function my_handler(data) {
 
 		console.log('success handler');
+		//console.log(data);
+
+		$('#fest-list-items').append(data);
 
 	};
 })
