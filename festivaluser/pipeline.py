b37545f8d24b7dff460d935f8147df7c8721ab15
Tim Claes <tim.claes@me.com>
2016-05-25 15:37:20 +0200

    Added custom pipeline to python-social-auth

--- festivaluser/pipeline.py
+++ festivaluser/pipeline.py
@@ -1,4 +1,15 @@
+from .models import FestivalAdvisorUser
 
 def save_profile(backend, user, response, *args, **kwargs):
+	'''
+	We dienen deze methode toe te voegen aan de default python social auth pipeline
+	om een User Profile (= FestivalAdvisorUser) te linken aan de nieuwe gebruiker
+	'''
 
-	print(jeej)
+	if kwargs['is_new']:
+		print('nieuwe aanmelding!')
+		new = FestivalAdvisorUser(user=user)
+		new.save()
+
+	else:
+		print('geen nieuwe!')
