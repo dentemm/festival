6fa31de91992ace750a7621184614a95a65ee56b
Tim Claes <tim.claes@me.com>
2016-05-26 12:00:46 +0200

    improvements to calendar page

---
+++ festival/static/js/lineupcloud.js
@@ -0,0 +1,14 @@
+        var words = [
+          {text: "Mumford And Sons", weight: 13},
+          {text: "Metallica", weight: 10.5},
+          {text: "Deus", weight: 9.4},
+          {text: "Evil Superstar", weight: 8},
+          {text: "StarFlam", weight: 6.2},
+          {text: "Radiohead", weight: 5},
+          {text: "Editors", weight: 5},
+          /* ... */
+        ];
+
+        $('#demo').jQCloud(words, {
+          autoResize: true
+        });
