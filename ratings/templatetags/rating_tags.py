f4385ad7b531a985084d248f93598587b8df9fc4
Tim Claes <tim.claes@me.com>
2016-05-27 20:59:29 +0200

    Need to do a lot of work on template tags

--- ratings/templatetags/rating_tags.py
+++ ratings/templatetags/rating_tags.py
@@ -5,6 +5,59 @@ from ..models import Score, Vote
 
 register = template.Library()
 
+class BaseRatingNode(template.Node):
+	'''
+	Deze base helper klasse wordt gebruikt voor de get_rating_* template tags
+	'''
+
+	def __init__(self, ctype=None, object_pk_expr=None, object_expr=None, as_varname=None, rating=None):
+		if ctype is None and object_expr is None:
+			raise template.TemplateSyntaxError(
+    			"Comment nodes must be given either a literal object or a ctype and object pk."
+    			)
+
+		self.as_varname = as_varname
+		self.ctype = ctype
+		self.object_pk_expr = object_pk_expr
+		self.object_expr = object_expr
+		self.rating = rating
+
+	@classmethod
+	def handle_token(cls, parser, token):
+
+		tokens = token.split_contents()
+
+		if tokens[1] != 'for':
+			raise template.TemplateSyntaxError('Tweede argument in %r tag moet "for" zijn' % tokens[0])
+
+
+		if len(tokens) == 5:
+			if tokens[3] != 'as':
+				raise template.TemplateSyntaxError('Vierde argument in %r tag moet "as" zijn' % tokens[0])
+
+			return cls(
+				object_expr=parser.compile_filter(tokens[2]),
+				as_varname=tokens[4]
+				)
+
+		else:
+			raise template.TemplateSyntaxError('voorlopig aanvaard tim enkel deze setup, kan nog aangepast worden')
+
+	def render(self, context):
+		qs = self.get_queryset(context)
+		context[self.as_varname] = self.get_context_value_from_queryset(context, qs)
+		return ''
+
+	def get_context_value_from_queryset(self, context, qs):
+		"""Subclasses should override this."""
+		raise NotImplementedError
+
+
+@register.simple_tag
+def get_rating_form(parser, token):
+
+	pass
+
 @register.simple_tag
 def votes_count(obj):
 	'''
