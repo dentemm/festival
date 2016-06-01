60137a83bafd8f82fc612004c2d21350d33a05aa
Tim Claes <tim.claes@me.com>
2016-06-01 19:01:17 +0200

    added management command to be able to save and publish all festival pages

---
+++ home/management/commands/saveandpublishall.py
@@ -0,0 +1,20 @@
+from django.core.management.base import BaseCommand, CommandError
+
+from home.models import FestivalPage
+
+class Command(BaseCommand):
+
+	help = 'Adss end date for all festivals'
+
+	def handle(self, *args, **options):
+
+		for festival in FestivalPage.objects.all():
+
+			latest = festival.get_latest_revision_as_page()
+
+			latest.save_revision().publish()
+
+			#page = festival.save_revision().publish()
+
+		self.stdout.write(self.style.SUCCESS('Het is gefixt!'))
+
