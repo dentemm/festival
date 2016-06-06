283dd7cd60b891980da6bdd45cf9a40c2a54bd50
Tim Claes <tim.claes@me.com>
2016-06-06 19:50:47 +0200

    Allow owl carousel navigation by page

--- festival/static/kevin/js/owlcalendar.js
+++ festival/static/kevin/js/owlcalendar.js
@@ -4,7 +4,11 @@ $('.owl-carousel').owlCarousel({
     autoWidth:true,
     items:6,
     autoPlay: 2500,
+    //autoPlay: false,
     slideSpeed: 1000,
     rewindSpeed: 850,
+    pagination: true,
+    //paginationNumbers: true,
+    scrollPerPage: true,
     //paginationSpeed: 8000,
 })
