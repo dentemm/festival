e44f6932282591ef0f0252dbb6a6f2179b40444c
Tim Claes <tim.claes@me.com>
2016-06-24 15:34:04 +0200

    activated debug mode

--- festival/settings/production.py
+++ festival/settings/production.py
@@ -3,7 +3,7 @@ from .base import *
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'bql1e+0p5eaee42i_)(ohve%sm65&1na6$#vm$pgn&@tem$&gs'
 
-DEBUG = False
+DEBUG = True
 
 try:
     from .local import *
