c72217be0389f377e0443ab639d6c08e28f0e93e
Tim Claes <tim.claes@me.com>
2016-07-10 11:30:28 +0200

    Added template tag to show ticket url if present

--- home/templatetags/home_tags.py
+++ home/templatetags/home_tags.py
@@ -1,10 +1,29 @@
 from django import template
 from django.contrib.contenttypes.models import ContentType
 
-from ..models import FestivalPageRateableAttribute, FestivalPage
+from ..models import FestivalPageRateableAttribute, FestivalPage, FestivalPageRelatedLink
 
 register = template.Library()
 
+
+@register.simple_tag
+def check_ticket_link(obj):
+
+	print('object: %s' % obj)
+
+	fest = FestivalPage.objects.get(pk=obj)
+
+	for link in obj.related_links.all():
+
+		print('link: %s' % link.title)
+
+		if link.title.lower() == 'tickets':
+
+			#print('jeeeeej')
+
+			return link.link_external
+
+	return ''
 
 @register.simple_tag
 def get_festival(object_id, content_type):
@@ -19,8 +38,7 @@ def get_festival(object_id, content_type):
 @register.simple_tag
 def get_attribute(obj):
 	'''
-	Deze template tag retourneert het totaal aantal gebruikers dat een stem heeft
-	uitgebracht op dit object
+	Deze template tag retourneert h
 
 	Gebruik {% get_attribute <obj> %}
 	'''
